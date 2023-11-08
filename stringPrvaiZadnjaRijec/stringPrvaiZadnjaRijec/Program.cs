f5cfc49ef7c123e43567d13093a8f25d091c9dd4
Non't <90344104+Shef905015@users.noreply.github.com>
2023-11-08 09:25:31 +0100

    Add files via upload

---
+++ stringPrvaiZadnjaRijec/stringPrvaiZadnjaRijec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringPrvaiZadnjaRijec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica;
+
+            Console.Write("Unesite neku recenicu: ");
+            recenica = Console.ReadLine();
+
+            Console.WriteLine("Ovo je prva rijec: " + recenica.Substring(0, recenica.IndexOf(" ")));
+            Console.WriteLine("Ovo je zadnja rijec: " + recenica.Substring(recenica.LastIndexOf(" ") + 1));
+
+            Console.ReadKey();
+        }
+    }
+}
